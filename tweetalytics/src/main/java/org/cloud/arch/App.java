3752cb8f186d739415bfa51b55396039f0c3a390
Karim Gawish <karimgawish.1@gmail.com>
2015-10-23 15:11:04 +0200

    Adding the mvn project structure.

---
+++ tweetalytics/src/main/java/org/cloud/arch/App.java
@@ -0,0 +1,13 @@
+package org.cloud.arch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
