7fe496309d82de03c2e56e70e0a514960618a9cb
Karim Gawish <karimgawish.1@gmail.com>
2015-11-21 14:12:15 +0100

    Making the analytics class.

---
+++ tweetalytics/src/main/java/fr/unice/polytech/Analytics.java
@@ -0,0 +1,35 @@
+package fr.unice.polytech;
+
+import java.util.List;
+
+public class Analytics {
+
+    public void startAnalytics(){
+
+    }
+
+    public void trendMatch(List<String> hashtags1, List<String> hashtags2){
+
+    }
+
+    public void localityMatch(String hashtag){
+
+    }
+
+    public void trendEvolution(Integer k){
+
+    }
+
+    public void watchdog(String hashtag){
+
+    }
+
+    public void tweetPropagation(){}
+
+    public void retweetBot(){}
+
+    public void stopAnalytics(){
+
+    }
+
+}
