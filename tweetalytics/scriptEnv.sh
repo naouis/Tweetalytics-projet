ba115f015507bde091c16610ea70baf31ca71869
Youssef Salim <salimyoussef11@yahoo.fr>
2015-11-21 16:18:58 +0100

    Script pour initialiser l'environement

--- tweetalytics/scriptEnv.sh
+++ tweetalytics/scriptEnv.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-eb init
-aws iam add-role-to-instance-profile --instance-profile-name aws-elasticbeanstalk-service-role --role sacc
+if [ "$#" != "1" ] 
+then
+	echo "Erreur: specifiez en argument le role IAM approprie"
+else
+	rm -R .elasticbeanstalk/
+	eb init < ./mesConfigs
+	aws iam add-role-to-instance-profile --instance-profile-name aws-elasticbeanstalk-ec2-role --role $1
+	eb create --service-role $1 < ./mesConfigs2
+fi
